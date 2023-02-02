bb18c4e60befc5ba7b2efea77b8ee357ca0c9c0f
sawyer tarr <sawyertarr@outlook.com>
2023-02-02 14:32:19 -0600

    (OperatorsExercises)

---
+++ OperatorsExercise/Exercise2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExercise
+{
+    internal class Exercise2
+    {
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI* radius * radius;
+            
+        }
+    }
+}
---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿//Exercise 1
+
+using OperatorsExercise;
+
+int num1 = 17;
+int num2 = 4;
+
+int quotient = num1 / num2;
+int remainder = num1% num2;
+
+if (num1 == 17 && num2 == 4)
+{
+    Console.WriteLine($"{num1}/{num2} is {quotient} remainder {remainder}");
+}
+
+//Exercise 2
+ 
+Console.WriteLine("What is the radius of your circle?");
+var radius = double.Parse(Console.ReadLine());
+
+double area = Exercise2.AreaOfCircle(radius);
+
+Console.WriteLine($"The area of a circle with a radius of {radius} is {area}.");
