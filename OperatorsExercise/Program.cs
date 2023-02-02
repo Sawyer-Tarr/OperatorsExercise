4343bc7bcdcbbfe6152860d8b6cb739ffeecd67a
sawyer tarr <sawyertarr@outlook.com>
2023-02-02 14:41:40 -0600

    BonusExercise

--- OperatorsExercise/Program.cs
+++ OperatorsExercise/Program.cs
@@ -1,4 +1,4 @@
-﻿//Exercise 1
+﻿////Exercise 1
 
 using OperatorsExercise;
 
@@ -6,7 +6,7 @@ int num1 = 17;
 int num2 = 4;
 
 int quotient = num1 / num2;
-int remainder = num1% num2;
+int remainder = num1 % num2;
 
 if (num1 == 17 && num2 == 4)
 {
@@ -14,10 +14,19 @@ if (num1 == 17 && num2 == 4)
 }
 
 //Exercise 2
- 
+
 Console.WriteLine("What is the radius of your circle?");
 var radius = double.Parse(Console.ReadLine());
 
 double area = Exercise2.AreaOfCircle(radius);
 
 Console.WriteLine($"The area of a circle with a radius of {radius} is {area}.");
+
+
+// Bonus
+
+var i = 3;
+var j = 4;
+var k = ++i * j++;
+
+Console.WriteLine(k); 
